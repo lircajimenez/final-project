4feacbf88a14e3130c873d5fe8dca9028ac51466
Lirca Jimenez <lirca.je@gmail.com>
2021-04-12 14:23:29 -0400

    global styles & header draft

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,12 +3,18 @@ import styled from "styled-components";
 
 const HomePage = () => {
   return (
-    <div>
+    <Container>
       <H1>HomePage</H1>
-    </div>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 const H1 = styled.h1`
   font-family: Arial, Helvetica, sans-serif;
   color: red;
