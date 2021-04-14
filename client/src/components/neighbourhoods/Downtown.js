1262dc69775b4fbe9869ed6766b4985f0d9d7f5d
Lirca Jimenez <lirca.je@gmail.com>
2021-04-14 12:11:08 -0400

    cloudinary setup

--- client/src/components/neighbourhoods/Downtown.js
+++ client/src/components/neighbourhoods/Downtown.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Image } from "cloudinary-react";
 
 const Downtown = () => {
+  const [images, setImages] = useState();
+
+  const loadImages = async () => {
+    try {
+      const res = await fetch("/montreal");
+      const data = await res.json();
+      setImages(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, []);
+
   return (
     <Container>
-      <H1>Downtown</H1>
+      <H1>Test gallery</H1>
+      <div className="gallery">
+        {images &&
+          images.map((image, index) => (
+            <Image
+              key={index}
+              cloudName="dec2frnoe"
+              publicId={image}
+              width="300"
+              height="auto"
+            />
+          ))}
+      </div>
     </Container>
   );
 };
